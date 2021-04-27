56f543adea1092d78a12142f089153102ca65917
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2021-04-27 15:23:34 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
     music.playMelody("F C F A F E A C ", 120)
     music.playMelody("C F C5 F A C5 C C ", 120)
     music.playMelody("C5 E D E A G C G ", 120)
     music.playMelody("C5 E D E A G C G ", 120)
     music.playMelody("C5 E C E F G C5 F ", 120)
     music.playMelody("A F A C5 C G E C ", 120)
+    music.playMelody("A D G C5 C G A F ", 120)
 })
