becaebc83bd23e36ddcc6ccf1159113ca987af6d
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2021-04-27 12:40:57 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    music.playMelody("F C F A F E A C ", 120)
+    music.playMelody("C F C5 F A C5 C C ", 120)
+    music.playMelody("C5 E D E A G C G ", 120)
+    music.playMelody("C5 E D E A G C G ", 120)
+    music.playMelody("C5 E C E F G C5 F ", 120)
+    music.playMelody("A F A C5 C G E C ", 120)
+})
